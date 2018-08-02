1333d8b13c0c1cab52b8ef2a8d7ff821663f03ea
madhead84 <mad_head@ua.fm>
2018-08-02 19:10:31 +0300

    JS commit

---
+++ js/task_basic.js
@@ -0,0 +1,31 @@
+var firstVariable;
+var secontVariable;
+var thirdVariable;
+
+var firstVariableNumber = 1;
+var secontVariableStrin = "cat";
+var thirdVariableBulean = true;
+var fourthVariableUbdefined;
+var fifthVariableNull = null;
+var sixthVariableArray = [0, 5, 26];
+var arreyWithAllVariables = [
+                            0,
+                            "dog",
+                            false,
+                            null,
+                            undefined,
+                            {health: 15, speed: 26},
+                            [3,8]
+                            ];
+var objectWithTwoPrimitivesAndTwoCompositeDataType = {
+    heroLevel: 26,
+    heroName: "Kowsh",
+    heroTesks: [findSomething, killBoss, findGold],
+    heroSkils: {
+        jump: true,
+        longJump: false,
+        runSpeed: 30,
+    }
+}
+
+
