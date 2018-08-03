d750e899101657131d9108587f5428f6629315ac
madhead84 <mad_head@ua.fm>
2018-08-03 13:54:48 +0300

    JS have done

--- js/task_basic.js
+++ js/task_basic.js
@@ -2,43 +2,53 @@ var firstVariable, secontVariable;
 var thirdVariable;
 
 var firstVariableNumber = 1;
-var secontVariableStrin = "cat";
+var secontVariableStrin = 'cat';
 var thirdVariableBulean = true;
-var fourthVariableUbdefined;
+var fourthVariableUndefined;
 var fifthVariableNull = null;
 var sixthVariableArray = [0, 5, 26];
 var arreyWithAllVariables = [
-                            0,
-                            "dog",
-                            false,
-                            null,
-                            undefined,
-                            {health: 15, speed: 26},
-                            [3,8]
-                            ];
+    0,
+    'dog',
+    false,
+    null,
+    undefined,
+    { health: 15, speed: 26 },
+    [3, 8]
+];
+
 var objectWithTwoPrimitivesAndTwoCompositeDataType = {
     heroLevel: 26,
-    heroName: "Kowsh",
-    heroTesks: ["findSomething", "killBoss", "findGold"],
+    heroName: 'Kowsh',
+    heroTesks: ['findSomething', 'killBoss', 'findGold'],
     heroSkils: {
         jump: true,
         longJump: false,
-        runSpeed: 30,
+        runSpeed: 30
     }
 };
 
 var firstArreyShopChecklist = [
-    {eags:{quontety:30}},
-    bread,
-    {water:{botels:2}},
-    {milk: {packages:1 }},
-    "kolbasa"];
+    {
+        eags: {
+            quontety: 30
+        }
+    },
+    'bread',
+    {
+        water: { botels: 2 }
+    },
+    {
+        milk: { packages: 1 }
+    },
+    'kolbasa'
+];
 
 var sasha = {
     ege: 25,
     height: 177,
-    profession: "Front end developer",
-    hobby: "Software engineering"
+    profession: 'Front-end developer',
+    hobby: 'Software engineering'
 };
 
 var tumbocka = {
@@ -51,17 +61,17 @@ var tumbocka = {
 };
 
 var pilesos = {
-    brand: "Philips",
-    power: "500W",
-    color: "green"
+    brand: 'Philips',
+    power: '500W',
+    color: 'green'
 };
 
 var phone = {
-    brand: "Apple",
-    screenResolution: "1200x800",
+    brand: 'Apple',
+    screenResolution: '1200x800',
     microUSB: false,
     lightning: true,
-    powerBattery: "3000mAh"
+    powerBattery: '3000mAh'
 };
 
 var pterodactel = {
@@ -72,12 +82,13 @@ var pterodactel = {
 
 var numbersArray = [2, 36, 18, 25, 16, 13, 7];
 var i = 0;
-while (i<numbersArray.length){
+while (i < numbersArray.length){
     console.log(numbersArray[i]*numbersArray[i]);
     i++;
 }
 
 var arreyThirdElement = [2, 6, 25];
+arreyThirdElement[2];
 function returnTheSecondElement() {
     return arreyThirdElement[2];
 }
@@ -92,18 +103,21 @@ var objectWithThreeProperties = {
     relationship: true,
     weight: 20,
     edge: 11
-
 };
 
 objectWithThreeProperties.edge;
 
 var a = [
-    "dog",
-    "eat",
+    'dog',
+    'eat',
     18,
     34,
-    [0,
-        [26,
-            {y: "server", x: "client"}],
-        [72,12]
-    ]];
+    [
+        0,
+        [
+            26,
+            { y: 'North', x: 'South' }
+        ],
+        [72, 12]
+    ]
+];
