c5016781d088393ce2b1f2b6e6a060b154192b69
madhead84 <mad_head@ua.fm>
2018-08-03 11:44:29 +0300

    JS commit

--- js/task_basic.js
+++ js/task_basic.js
@@ -1,5 +1,4 @@
-var firstVariable;
-var secontVariable;
+var firstVariable, secontVariable;
 var thirdVariable;
 
 var firstVariableNumber = 1;
@@ -20,7 +19,7 @@ var arreyWithAllVariables = [
 var objectWithTwoPrimitivesAndTwoCompositeDataType = {
     heroLevel: 26,
     heroName: "Kowsh",
-    heroTesks: [findSomething, killBoss, findGold],
+    heroTesks: ["findSomething", "killBoss", "findGold"],
     heroSkils: {
         jump: true,
         longJump: false,
@@ -28,4 +27,47 @@ var objectWithTwoPrimitivesAndTwoCompositeDataType = {
     }
 }
 
+var firstArreyShopChecklist= [
+    {eags:{quontety:30}},
+    bread,
+    {water:{botels:2}},
+    {milk: {packages:1 }},
+    "kolbasa"]
 
+var numbersArey = [2, 36, 18, 25, 16, 13, 7];
+var i = 0;
+while (i<numbersArey.length){
+    console.log(numbersArey[i]*numbersArey[i]);
+    i++;
+}
+
+var arreyThirdElement = [2, 6, 25];
+function returnTheSecondElement() {
+    return arreyThirdElement[2];
+}
+
+//modified previus task
+
+function retunNecessaryElementOfArrey(inputArray, necessaryElementOfArrey) {
+    return inputArray[necessaryElementOfArrey-1];
+}
+
+var objectWithThreeProperties = {
+    relationship: true,
+    weight: 20,
+    edge: 11
+
+}
+
+objectWithThreeProperties.edge;
+
+var a = [
+    "dog",
+    "eat",
+    18,
+    36,
+    [0,
+        [26,
+            {y: "server", x: "client"}],
+        [26,16]
+    ]];
