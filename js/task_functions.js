b27ec4be1cb2705f53609d2415735b955a677f18
madhead84 <mad_head@ua.fm>
2018-08-13 12:39:27 +0300

    JS functions

--- js/task_functions.js
+++ js/task_functions.js
@@ -1,12 +1,12 @@
-var first  = function(array) {
+var first = function (array) {
     return array[0];
 };
 
-var last = function(array) {
-    return array[array.length-1];
+var last = function (array) {
+    return array[array.length - 1];
 };
 
-var min = function(array) {
+var min = function (array) {
     var min = array[0];
     for (var i = 0; i < array.length; i++) {
         if (array[i] < min) {
@@ -16,7 +16,7 @@ var min = function(array) {
     return min;
 };
 
-var max = function(array) {
+var max = function (array) {
     var max = array[0];
     for (var i = 0; i < array.length; i++) {
         if (array[i] > max) {
@@ -26,7 +26,7 @@ var max = function(array) {
     return max;
 };
 
-var compact = function(array) {
+var compact = function (array) {
     var result = [];
     for (var i = 0; i < array.length; i++) {
         if (array[i] !== undefined) {
@@ -36,7 +36,7 @@ var compact = function(array) {
     return result;
 };
 
-var contains = function(array, value) {
+var contains = function (array, value) {
     for (var i = 0; i < array.length; i++) {
         if (array[i] === value) {
             return true;
@@ -45,32 +45,32 @@ var contains = function(array, value) {
     return false;
 };
 
-var isUndefined = function(value) {
+var isUndefined = function (value) {
     if (value === undefined) {
         return true;
     }
     return false;
 };
 
-var size = function(array) {
+var size = function (array) {
     return array.length;
 };
 
-var isNull = function(value) {
+var isNull = function (value) {
     if (value === null) {
         return true;
     }
     return false;
 };
 
-var isBoolean = function(value) {
+var isBoolean = function (value) {
     if (value === true || value === false) {
         return true;
     }
     return false;
 };
 
-var without = function(array, value) {
+var without = function (array, value) {
     var result = [];
     for (var i = 0; i < array.length; i++) {
         if (array[i] !== value) {
@@ -80,7 +80,7 @@ var without = function(array, value) {
     return result;
 };
 
-var reverse = function(array) {
+var reverse = function (array) {
     var result = [];
     for (var i = array.length - 1; i >= 0; i--) {
         result.push(array[i]);
@@ -88,13 +88,14 @@ var reverse = function(array) {
     return result;
 };
 
-var join = function(array, divider) {
+var join = function (array, divider) {
     var result = '';
     if (divider === undefined) {
         divider = ',';
-    };
-    for (var i = 0; i < array.length; i++){
-        result +=  array[i] + divider;
+    }
+    ;
+    for (var i = 0; i < array.length; i++) {
+        result += array[i] + divider;
     }
     return result;
 };
@@ -102,7 +103,7 @@ var join = function(array, divider) {
 !!!!!
 var indexOf = function (array, value) {
     var returnedFirstIndexl;
-    for (var i = 0; i < array.length; i++){
+    for (var i = 0; i < array.length; i++) {
         if (array[i] === value) {
             returnedFirstIndex = i;
             break;
@@ -113,7 +114,7 @@ var indexOf = function (array, value) {
 !!!!!!
 var lastIndexOf = function (array, value) {
     var returnedLastIndex;
-    for (var i = array.length; i >= 0; i--){
+    for (var i = array.length; i >= 0; i--) {
         if (array[i] === value) {
             returnedLastIndex = i;
             break;
@@ -122,31 +123,31 @@ var lastIndexOf = function (array, value) {
     return returnedLastIndex === undefined ? -1 : returnedLastIndex;
 };
 !!!
-var concat = function(array1, array2) {
+var concat = function (array1, array2) {
     var result = [];
-    for (var i = 0; i <array1.length; i++) {
+    for (var i = 0; i < array1.length; i++) {
         result[result.length] = array1[i];
     }
-    for (var i = 0; i <array2.length; i++) {
+    for (var i = 0; i < array2.length; i++) {
         result[result.length] = array2[i];
     }
     return result;
 };
 
-var push = function(array, value) {
+var push = function (array, value) {
     array[array.length] = value;
     //return array;
 };
 
-var sum = function(array) {
+var sum = function (array) {
     var sum = 0;
     for (var i = 0; i < array.length; i++) {
-         sum += array[i];
+        sum += array[i];
     }
     return sum;
 };
 
-var multiply = function(array) {
+var multiply = function (array) {
     var multiply = 1;
     for (var i = 0; i < array.length; i++) {
         multiply *= array[i];
@@ -154,7 +155,7 @@ var multiply = function(array) {
     return multiply;
 };
 
-var keys = function(object) {
+var keys = function (object) {
     var result = [];
     for (var key in object) {
         result[result.length] = key;
@@ -162,7 +163,7 @@ var keys = function(object) {
     return result;
 };
 
-var values = function(object) {
+var values = function (object) {
     var result = [];
     for (var key in object) {
         result[result.length] = object[key];
@@ -170,7 +171,7 @@ var values = function(object) {
     return result;
 };
 
-var pairs = function(object) {
+var pairs = function (object) {
     var result = [];
     for (var key in object) {
         result[result.length] = [key, object[key]];
@@ -178,7 +179,7 @@ var pairs = function(object) {
     return result;
 };
 
-var invert = function(object) {
+var invert = function (object) {
     var result = {};
     for (var key in object) {
         result[object[key]] = key;
@@ -187,10 +188,10 @@ var invert = function(object) {
 };
 
 !!!!!!!
-var omit = function(object, removeValue) {
+var omit = function (object, removeValue) {
     var result = {};
     for (var key in object) {
-        if (key === removeValue);
+        if (key === removeValue) ;
         else {
             result[key] = object[key];
         }
@@ -199,17 +200,17 @@ var omit = function(object, removeValue) {
 };
 
 !!!!
-var has = function(object, key) {
+var has = function (object, key) {
     return (key in object);
 };
 
-var isMatch = function(object1, object2) {
+var isMatch = function (object1, object2) {
     for (var key in object2) {
         return object1[key] === object2[key];
     }
 };
 
-var isEmpty = function(collection) {
+var isEmpty = function (collection) {
     for (var key in collection) {
         return false;
     }
@@ -220,41 +221,41 @@ var isEmpty = function(collection) {
 var uniq = function (array) {
     var returnedArray = [];
     goto:
-    for (var i = 0; i < array.length; i++) {
-        for (var j = 0; j < returnedArray.length; j++) {
-            if ( array[j] === array[i]) continue goto;
+        for (var i = 0; i < array.length; i++) {
+            for (var j = 0; j < returnedArray.length; j++) {
+                if (array[j] === array[i]) continue goto;
+            }
+            returnedArray[returnedArray.length] = array[i];
         }
-        returnedArray[returnedArray.length] = array[i];
-    }
     return returnedArray;
 };
 
 //22222222222222222222222222222222222222222222222
 var sort = function (array) {
-    for (var i = 0; i <array.length ; i++) {
+    for (var i = 0; i < array.length; i++) {
         for (var j = 0; j < (array.length - 1); j++) {
-            if (array[j] > array[j+1]) {
+            if (array[j] > array[j + 1]) {
                 var max = array[j];
-                var min = array[j+1];
+                var min = array[j + 1];
                 array[j] = min;
-                array[j+1] = max;
+                array[j + 1] = max;
             }
         }
     }
     return array;
 };
 
-var getMatrixSum = function(array) {
+var getMatrixSum = function (array) {
     var sum = 0;
-    for(var i = 0; i < array.length; i++){
-        for(var j = 0; j < array[i].length; j++){
+    for (var i = 0; i < array.length; i++) {
+        for (var j = 0; j < array[i].length; j++) {
             sum += array[i][j];
         }
     }
     return sum;
 };
 
-var getMatrixSumByDiagonal = function(array) {
+var getMatrixSumByDiagonal = function (array) {
     var sum = 0;
     for (i = 0; i < array.length; i++) {
         sum += array[i][i];
@@ -262,13 +263,13 @@ var getMatrixSumByDiagonal = function(array) {
     return sum;
 };
 
-var each = function(array, iteratee) {
+var each = function (array, iteratee) {
     for (var i = 0; i < array.length; i++) {
         iteratee(array[i], i)
     }
 };
 
-var map = function(array, iteratee) {
+var map = function (array, iteratee) {
     var result = [];
     for (var i = 0; i < array.length; i++) {
         result[result.length] = iteratee(array[i]);
@@ -278,96 +279,110 @@ var map = function(array, iteratee) {
 
 var intersection = function (array1, array2) {
     var returnedArray = [];
-        for (var i = 0; i < array1.length; i++) {
-            if ( array1[i] === array2[i]) {
-                    returnedArray[returnedArray.length] = array1[i];
-                }
-            }
+    for (var i = 0; i < array1.length; i++) {
+        if (array1[i] === array2[i]) {
+            returnedArray[returnedArray.length] = array1[i];
+        }
+    }
     return returnedArray;
 };
 
-var findIndex = function(array, predicate){
+var findIndex = function (array, predicate) {
     for (var i = 0; i < array.length; i++) {
         if (predicate(array[i]) === true) return i;
     }
     return -1;
 };
 
+var find = function (list, predicate) {
+    //for (var i = 0 ; i < list.length; i++)
+    for (var i in list) {
+        if (predicate(list[i]) === true) return list[i];
+    }
+    return undefined;
+};
 
+var filter = function (list, predicate) {
+    var result = [];
+    for (var i in list) {
+        if (predicate(list[i]) === true) {
+            result[result.length] = list[i];
+        }
+    }
+    return result;
+};
 
+var reject = function (list, predicate) {
+    var result = [];
+    for (var i in list) {
+        if (predicate(list[i]) === false) {
+            result[result.length] = list[i];
+        }
+    }
+    return result;
+};
 
-Создать функцию find с двумя входными параметрами (массив list и функция predicate). Функция find вызывает для каждого элемента list
-функцию сравнения predicate, возвращая первый элемент, для которого predicate вернула true, или undefined, если ни один элемент не подошёл.
-Пример работы:
-find([1, 2, 3, 4, 5, 6], function(num) { return num % 2 == 0; });
-// => 2
-
-
-
-
-Создать функцию filter с двумя входными параметрами (массив list и функция predicate) которая проходит через каждое значение list,
-    возвращая массив всех значений, для которых predicate вернул true.
-Пример работы:
-filter([1, 2, 3, 4, 5, 6], function(num) { return num % 2 == 0; });
-// => [2, 4, 6]
-
-
-
-
-Создать функцию reject с двумя входными параметрами (массив list и функция predicate) которая возвращает массив, содержащий все значения list,
-за исключением элементов, для которых функция predicate вернула значение true. Т.е. reject является «антонимом» filter.
-Пример работы:
-reject([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
-// => [1, 3, 5]
-
-
-
-
-Создать функцию every с двумя входными параметрами (массив list и функция predicate). Вернёт true, если для каждого значения из list predicate вернёт true.
-Пример работы:
-every([2, 4, 5], function(num) { return num % 2 == 0; });
+//что возращать для пустого масива, null, 0?
+// Создать функцию every с двумя входными параметрами (массив list и функция predicate). Вернёт true, если для каждого значения из list predicate вернёт true.
+// Пример работы:
+//  every([2, 4, 5], function(num) { return num % 2 == 0; });
 // => false
 
+var every = function (list, predicate) {
+    for (var i in list) {
+        if (predicate(list[i]) !== true) return false;
+    }
+    return true;
+};
 
+var some = function (list, predicate) {
+    for (var i in list) {
+        if (predicate(list[i]) === true) return true;
+    }
+    return false;
+};
 
+var partition = function (array, predicate) {
+    var paired = [];
+    var unpaired = [];
+    var result = [];
+    for (var i = 0; i < array.length; i++) {
+        if (predicate(array[i]) === true) {
+            paired[paired.length] = array[i];
+        }
+        else {
+            unpaired[unpaired.length] = array[i];
+        }
+    }
+    result[0] = paired;
+    result[1] = unpaired;
+    return result;
+};
 
-Создать функцию some с двумя входными параметрами (массив list и функция predicate). Вернёт true, если хотя бы для одного значения из list predicate вернёт true.
-Пример работы:
-some([2, 4, 5], function(num) { return num % 2 == 0; });
-// => true
+var extend = function (destination, source) {
+    var result = destination;
+    for (var key in source) {
+        result[key] = source[key];
+    }
+    return result;
+};
 
+var defaults = function (object, defaultO) {
+    var result = object;
+    for (var key in defaultO) {
+        if (key in object === false) {
+            result[key] = defaultO[key]
+        }
+    }
+    return result;
+};
 
-
-
-Создать функцию partition с двумя входными параметрами (массив array и функция predicate). Разобъёт массив array на две части:
-одна - для элементов которой функция predicate вернёт true, и другая - для всех остальных.
-Пример работы:
-partition([0, 1, 2, 3, 4, 5], function(num) { return num % 2 == 0; });
-// => [[0, 2, 4], [1, 3, 5]]
-
-
-
-
-Создать функцию extend с двумя входными параметрами (объект destination и объект source). Скопирует все свойства из объекта
-source в объект destination. Если объект source имеет одноименные свойства с объектом destination, то значения destination будут затёрты значениями из source.
-Пример работы:
-extend({name: 'moe'}, {age: 50});
-// => {name: 'moe', age: 50}
-
-
-
-
-Создать функцию defaults с двумя входными параметрами (объект object и объект default). Функция defaults проинициализирует неопределённые (undefined)
-свойства объета значениями одноимённых свойств из default. Если же какие-то свойства объекта уже определены, то они не будут изменены.
-Пример работы:
-defaults({flavor: "chocolate"}, {flavor: "vanilla", sprinkles: "lots"});
-// => {flavor: "chocolate", sprinkles: "lots"}
-
-
-
-
-Создать функцию random с двумя (или одним) входными параметрами (min, max). Возвращает случайное целое число, в диапазоне от min до max,
-включительно. Если вы передали только один агрумент, будет использован диапазон от 0 до переданного числа (т.е. до max).
-Пример работы:
-random(0, 100);
-// => 42
+var random = function (min, max) {
+    var result;
+    if (max === undefined) {
+        max = min;
+        min = 0;
+    }
+    result = Math.round(min - 0.5 + Math.random() * (max - min + 1));
+    return result;
+};
