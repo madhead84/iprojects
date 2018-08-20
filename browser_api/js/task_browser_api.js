811a90047eae13e179d8e005fc01fb9508faf2ff
madhead84 <mad_head@ua.fm>
2018-08-20 18:58:44 +0300

    mario

--- browser_api/js/task_browser_api.js
+++ browser_api/js/task_browser_api.js
@@ -55,7 +55,6 @@ document.querySelector('.b11').addEventListener('click', function () {
 });
 
 document.body.addEventListener('click', function () {
-    console.log(event.currentTarget);
     document.querySelector('.t12').innerHTML = 'хуй';
 
 });
@@ -63,3 +62,85 @@ document.body.addEventListener('click', function () {
 document.querySelector('.b13').addEventListener('click', function () {
     document.querySelector('.t13').innerHTML='Sun';
 });
+
+document.querySelector('.b14').addEventListener('click', function () {
+    var newWhore =  document.createElement('div');
+    newWhore.setAttribute('class', 'whore whore_jasmin');
+    document.querySelector('.whores-container').appendChild(newWhore);
+    document.querySelector('.whore_jasmin').innerHTML =
+        '<div class="whore-name">'+ whore.name +'</div>' +
+        '<img src= '+ whore.photo +' width="200">' +
+        '<div class="whore-age">Возраст: '+ whore.age +'</div>' +
+        '<div class="whore-boobs">Размер груди: '+ whore.boobs +'</div>' +
+        '<div class="whore-height">Рост: '+ whore.height +'</div>' +
+        '<div class="whore-weight">Вес: '+ whore.heigh +'</div>'+
+        '<div class="whore-phone">'+ whore.phone +'</div>' +
+        '<div class="whore-can-come">Выезд: '+ (whore.can_come ? 'Да' : '-') +'</div>';
+});
+
+document.querySelector('.b15').addEventListener('click', function () {
+    document.querySelector('.cat-container-1').innerHTML =
+        '<img class="cat" src="images/kat.png" width="100">' + '<img class="cat" src="images/kat.png" width="100">';
+    document.querySelector('.cat-container-2').innerHTML=
+        '<img class="fish" src="images/fish.jpg" width="100">';
+});
+
+document.querySelector('.b16').addEventListener('click', function () {
+    document.querySelector('.tooth').style.display = 'none';
+    document.querySelector('.boy').style.display = 'block';
+});
+
+document.querySelector('.b17').addEventListener('click', function() {
+    for (var i = 0; i < map.length; i++) {
+        for (var j = 0; j < map[i].length; j++) {
+            var top = i * 16;
+            var left = j * 16;
+            var el = document.createElement('div');
+            if (map[i][j] === 'z'){
+                el.setAttribute('class', 'tile x_z');
+            }
+            if (map[i][j] === 'k'){
+                el.setAttribute('class', 'tile x_k');
+            }
+            if (map[i][j] === 'c'){
+                el.setAttribute('class', 'tile x_c');
+             }
+            if (map[i][j] === 't'){
+                el.setAttribute('class', 'tile x_t');
+            }
+            if (map[i][j] === 'q'){
+                el.setAttribute('class', 'tile x_q');
+            }
+            if (map[i][j] === 'b'){
+                el.setAttribute('class', 'tile x_b');
+            }
+            if (map[i][j] === 'd'){
+                el.setAttribute('class', 'tile x_d');
+            }
+            if (map[i][j] === 'w'){
+                el.setAttribute('class', 'tile x_w');
+            }
+            if (map[i][j] === '') {
+                el.setAttribute('class', 'tile');
+            }
+            el.style.top = top +'px';
+            el.style.left = left +'px';
+            document.querySelector('.scene').appendChild(el);
+        }
+    }
+});
+
+
+
+
+
+var resultHTML = '';
+for (var i = 0; i < mikki.length; i++) {
+    for (var j = 0; j < mikki[i].length; j++) {
+        if (mikki[i][j] === 'X'){
+            resultHTML += '<div class="mikki_tile" style ="background: #000000" ></div>'
+        }
+        else resultHTML += '<div class="mikki_tile"></div>'
+    }
+}
+document.querySelector('.mikki_tiles').innerHTML = resultHTML;
