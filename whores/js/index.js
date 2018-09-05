90de91a50730117c0f0890dd18f23f88c1758dbe
madhead84 <mad_head@ua.fm>
2018-09-05 18:52:58 +0300

    RegExp

--- whores/js/index.js
+++ whores/js/index.js
@@ -1,36 +1,78 @@
-var listView = {};
-var addListView = {};
-var updateListView = {};
+// localStorage.setItem("Ключ", "Значение");
+// localStorage.getItem("Ключ");
+// localStorage.removeItem("Ключ");
+// var tostring = JSON.stringify(obj);
+// localStorage.setItem("myKey", serialObj);
+// var toOject = JSON.parse('строкаШлюх');
 
-var init = function () {
+var whoresCollection = {
+
+    init: function() {
+        // get data from localStorage
+    },
+
+    attributes: [],
+
+    getWhoreByID: function() {},
+
+    removeWhoreByID: function() {}
 
 };
 
-var  render  = function () {
+var listView = {
+
+    init: function() {},
+
+    render: function() {},
+
+    subscribe: function() {}
 
 };
 
-var subscribe = function () {
+var addFormView = {
+
+    tmplFn: doT.template($('#add-form-template').html()),
+
+    init: function() {
+        this.render();
+        this.subscribe();
+    },
+
+    render: function() {
+        $('.add-form-container').html(this.tmplFn());
+    },
+
+    subscribe: function() {
+        $('.add').on('click', function() {
+            var a = this.getFormData();
+            console.log(a);
+        }.bind(this));
+    },
+
+    getFormData: function() {
+        return {
+            firstName: $('.add-form .first-name').val(),
+            lastName: $('.add-form .last-name').val(),
+            age: $('.add-form .age').val(),
+            price: $('.add-form .price').val(),
+            id: this.getUniqID()
+        };
+    },
+
+    getUniqID: function() {
+        return '_' + Math.random().toString(36).substr(2, 9);
+    }
 
 };
 
-localStorage.setItem("Ключ", "Значение");
-localStorage.getItem("Ключ");
-localStorage.removeItem("Ключ");
-var serialObj = JSON.stringify(obj);
-localStorage.setItem("myKey", serialObj);
+addFormView.init();
 
-var toOject = JSON.parse(строкаШлюх);
+var editFormView = {
 
-var shluha = {
-    id: '',
-    name: '',
-    last_name: '',
-    age: '',
-    price: ''
+    init: function() {},
+
+    render: function() {},
+
+    subscribe: function() {}
+
 };
-
-var ID = function () {
-    return '_' + Math.random().toString(36).substr(2, 9);
-};
-
