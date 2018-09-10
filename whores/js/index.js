effd2c922aeb93a1175c4821abe195a39aa481bf
madhead84 <mad_head@ua.fm>
2018-09-10 19:59:09 +0300

    RegExp

--- whores/js/index.js
+++ whores/js/index.js
@@ -2,15 +2,41 @@ var whoresCollection = {
     init: function() {
         this.attributes = JSON.parse(localStorage.getItem('whores')) || [];
     },
+
     add: function(item) {
         this.attributes.push(item);
+        this.updateStorage();
+    },
+
+    attributes: [],
+
+    getWhoreByID: function(id) {
+        return _.findWhere(this.attributes, {id: id});
+    },
+
+    removeWhoreByID: function(id) {
+        this.attributes = _.without(this.attributes, this.attributes[_.findIndex(this.attributes, {id: id})]);
+        this.updateStorage();
+    },
+
+    updateWhore: function (whore) {
+        whoreIndex = _.findIndex(this.attributes, {id: whore.id});
+        this.attributes[whoreIndex] = whore;
+        this.updateStorage();
+    },
+
+    updateStorage: function() {
         localStorage.setItem('whores', JSON.stringify(this.attributes));
     },
-    attributes: [],
-    getWhoreByID: function(id) {
 
-    },
-    removeWhoreByID: function(id) {
+    showMore500: function () {
+        var result = [];
+        for (var i = 0; i < this.attributes.length; i++) {
+            if (this.attributes[i].price >= 500) {
+                result[result.length] = this.attributes[i];
+            }
+        }
+        return result;
     }
 };
 
@@ -18,32 +44,49 @@ whoresCollection.init();
 
 var listView = {
     tmplFn: doT.template($('#whores-template').html()),
-    init: function() {
-        this.render();
-    },
+
     render: function() {
         $('.whores-container').html(this.tmplFn(whoresCollection.attributes));
     },
-    subscribe: function() {}
+
+    subscribe: function() {
+        $('.whore').on('click', function(event) {
+            var id =  $(event.currentTarget).attr('data-id');
+            editFormView.render(whoresCollection.getWhoreByID(id));
+            addFormView.remove();
+        });
+    }
 };
 
-listView.init();
+listView.render();
 
 var addFormView = {
     tmplFn: doT.template($('#add-form-template').html()),
-    init: function() {
-        this.render();
-    },
+
     render: function() {
         $('.add-form-container').html(this.tmplFn());
         this.subscribe();
     },
+
     subscribe: function() {
         $('.add').on('click', function() {
             whoresCollection.add(this.getFormData());
-            listView.init();
+            listView.render();
+            this.resetForm();
         }.bind(this));
     },
+
+    resetForm: function() {
+        $('.add-form .first-name').val('');
+        $('.add-form .last-name').val('');
+        $('.add-form .age').val('');
+        $('.add-form .price').val('');
+    },
+
+    remove: function() {
+        $('.add-form-container').html('');
+    },
+
     getFormData: function() {
         return {
             firstName: $('.add-form .first-name').val(),
@@ -51,39 +94,53 @@ var addFormView = {
             age: $('.add-form .age').val(),
             price: $('.add-form .price').val(),
             id: this.getUniqID()
-        };
+        }
     },
 
     getUniqID: function() {
         return '_' + Math.random().toString(36).substr(2, 9);
     }
 };
-addFormView.init();
+
+addFormView.render();
 
 var editFormView = {
     tmplFn: doT.template($('#edit-form-template').html()),
-    init: function() {
-        this.render();
-    },
-    render: function() {
-        $('.edit-form-container').html(this.tmplFn());
+
+    render: function(whore) {
+        $('.edit-form-container').html(this.tmplFn(whore));
         this.subscribe();
     },
+
     subscribe: function() {
-        $('.edit').on('click', function() {
-            whoresCollection.add(this.getFormData());
-            listView.init();
+        $('.save').on('click', function() {
+            whoresCollection.updateWhore(this.getFormData());
+            this.remove();
+            listView.render();
+            addFormView.render();
+        }.bind(this));
+
+        $('.delete').on('click', function () {
+            var whoreId = $('.edit-form [name="id"]').val();
+            whoresCollection.removeWhoreByID(whoreId);
+            listView.render();
+            this.remove();
+            addFormView.render();
         }.bind(this));
     },
-    getFormData: function() {
-        return {
-            firstName: $('.add-form .first-name').val(),
-            lastName: $('.add-form .last-name').val(),
-            age: $('.add-form .age').val(),
-            price: $('.add-form .price').val(),
-        };
+
+    remove: function() {
+        $('.edit-form-container').html('');
     },
 
-};
+    getFormData: function() {
+        return {
+            id: $('[name="id"]').val(),
+            firstName: $('.edit-form .first-name').val(),
+            lastName: $('.edit-form .last-name').val(),
+            age: $('.edit-form .age').val(),
+            price: $('.edit-form .price').val(),
+        };
+    }
 
-editFormView.init();
+};
